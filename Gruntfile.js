fbc23d4f1a0cdfdf074163f2ac6e76e3007fffb4
pronin <denis.pronin@sibers.com>
2015-04-01 10:31:25 +0600

    format gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,374 +1,374 @@
 'use strict';
 
 module.exports = function (grunt) {
-    require('load-grunt-tasks')(grunt);
+  require('load-grunt-tasks')(grunt);
 
-    grunt.initConfig({
-        tempdir: '.tmp',
-        distdir: 'dist',
-        pkg: grunt.file.readJSON('package.json'),
-        src: {
-            js: ['src/app/voicebaseApp.js', 'src/**/*.js'],
-            jsVendor: [
-                'bower_components/api-console-voicebase/dist/scripts/api-console-vendor.js',
-                'bower_components/angular-route/angular-route.js',
-                'bower_components/api-console-voicebase/dist/scripts/api-console.js'
-            ],
-            html: ['src/index.html'],
-            scss: [
-                'bower_components/api-console-voicebase/dist/styles/api-console-dark-theme.css',
-                'bower_components/api-console-voicebase/dist/styles/api-console-light-theme.css'
-            ],
-            scssWatch: ['src/scss/**/*.scss'],
-            test: ['test/**/*.js']
-        },
+  grunt.initConfig({
+    tempdir: '.tmp',
+    distdir: 'dist',
+    pkg: grunt.file.readJSON('package.json'),
+    src: {
+      js: ['src/app/voicebaseApp.js', 'src/**/*.js'],
+      jsVendor: [
+        'bower_components/api-console-voicebase/dist/scripts/api-console-vendor.js',
+        'bower_components/angular-route/angular-route.js',
+        'bower_components/api-console-voicebase/dist/scripts/api-console.js'
+      ],
+      html: ['src/index.html'],
+      scss: [
+        'bower_components/api-console-voicebase/dist/styles/api-console-dark-theme.css',
+        'bower_components/api-console-voicebase/dist/styles/api-console-light-theme.css'
+      ],
+      scssWatch: ['src/scss/**/*.scss'],
+      test: ['test/**/*.js']
+    },
 
-        connect: {
-            options: {
-                hostname: '0.0.0.0',
-                port:     9000
-            },
+    connect: {
+      options: {
+        hostname: '0.0.0.0',
+        port: 9000
+      },
 
-            livereload: {
-                options: {
-                    livereload: true,
-                    open:       true,
-                    middleware: function (connect) {
-                        return [
-                            connect.static('dist')
-                        ];
-                    }
-                }
-            },
-
-            regression: {
-                options: {
-                    livereload: true,
-                    open:       false,
-                    middleware: function (connect) {
-                        return [
-                            connect.static('dist'),
-                            connect.static('test/regression/assets')
-                        ];
-                    }
-                }
-            }
-
-        },
-
-        clean: {
-            build: [
-                '<%= tempdir %>',
-                '<%= distdir %>'
-            ]
-        },
-
-        copy: {
-            assets: {
-                files: [{
-                    dest:   '<%= distdir %>',
-                    cwd:    'bower_components/api-console-voicebase/src/assets/',
-                    expand: true,
-                    src:    [
-                        '**',
-                        '!styles/**/*'
-                    ]
-                }]
-            },
-
-            pages: {
-                files: [{
-                    dest:   '<%= distdir %>/pages',
-                    cwd:    'src/assets/pages/',
-                    expand: true,
-                    src:    [
-                        '**'
-                    ]
-                }]
-            },
-
-            images: {
-              files: [{
-                dest:   '<%= distdir %>/img',
-                cwd:    'src/assets/img/',
-                expand: true,
-                src:    [
-                  '**'
-                ]
-              }]
-            }
-        },
-
-        ngtemplates: {
-            ramlConsole: {
-                options: {
-                    module: 'ramlConsoleApp'
-                },
-
-                cwd:  'src/app',
-                src:  '**/*.tpl.html',
-                dest: '<%= tempdir %>/templates/app.js'
-            }
-        },
-
-        concat: {
-            app: {
-                dest: '<%= distdir %>/scripts/api-console.js',
-                src:  [
-                    '<%= src.js %>',
-                    '<%= ngtemplates.ramlConsole.dest %>'
-                ]
-            },
-
-            index: {
-                options: {
-                    process: true
-                },
-
-                dest: '<%= distdir %>/index.html',
-                src:  'src/voicebase.html'
-            },
-
-            indexMulesoft: {
-                options: {
-                    process: true
-                },
-
-                dest: '<%= distdir %>/index.html',
-                src:  'src/index.html'
-            },
-
-            darkTheme: {
-                options: {
-                    process: function process(value) {
-                        return value.replace(/\.raml-console-CodeMirror/g, '.CodeMirror');
-                    }
-                },
-
-                dest: '<%= distdir %>/styles/api-console-dark-theme.css',
-                src:  [
-                    'bower_components/api-console-voicebase/src/assets/styles/vendor/codemirror.css',
-                    'bower_components/api-console-voicebase/src/assets/styles/fonts.css',
-                    'bower_components/api-console-voicebase/src/assets/styles/error.css',
-                    '<%= distdir %>/styles/voicebase-error.css',
-                    '<%= distdir %>/styles/api-console-dark-theme.css',
-                    'bower_components/api-console-voicebase/src/assets/styles/vendor/codemirror-dark.css'
-                ]
-            },
-
-            lightTheme: {
-                options: {
-                    process: function process(value) {
-                        return value.replace(/\.raml-console-CodeMirror/g, '.CodeMirror');
-                    }
-                },
-
-                dest: '<%= distdir %>/styles/api-console-light-theme.css',
-                src:  [
-                    'bower_components/api-console-voicebase/src/assets/styles/vendor/codemirror.css',
-                    'bower_components/api-console-voicebase/src/assets/styles/fonts.css',
-                    'bower_components/api-console-voicebase/src/assets/styles/error.css',
-                    '<%= distdir %>/styles/voicebase-error.css',
-                    '<%= distdir %>/styles/api-console-light-theme.css',
-                    'bower_components/api-console-voicebase/src/assets/styles/vendor/codemirror-light.css'
-                ]
-            },
-
-            vendor: {
-                src:  '<%= src.jsVendor %>',
-                dest: '<%= distdir %>/scripts/api-console-vendor.js'
-            }
-
-        },
-
-        concurrent: {
-            build: [
-                'build:scripts',
-                'concat:vendor',
-                'concat:index',
-                'copy:assets',
-                'copy:pages',
-                'copy:images',
-                'build:styles'
-            ],
-
-            buildMulesoft: [
-                'build:scripts',
-                'concat:vendor',
-                'concat:indexMulesoft',
-                'copy:assets',
-                'build:styles'
-            ],
-
-            themes: [
-                'concat:darkTheme',
-                'concat:lightTheme'
-            ]
-        },
-
-        sass: {
-            build: {
-                options: {
-                    sourcemap: 'none',
-                    style:     'expanded'
-                },
-
-                files: {
-                    '<%= distdir %>/styles/voicebase-error.css': 'src/scss/voicebase-error.scss',
-                    '<%= distdir %>/styles/api-console-light-theme.css': 'src/scss/light-theme.scss',
-                    '<%= distdir %>/styles/api-console-dark-theme.css':  'src/scss/dark-theme.scss'
-                }
-            },
-
-            min: {
-                options: {
-                    sourcemap: 'none',
-                    style:     'compressed'
-                },
-
-                files: {
-                    '<%= distdir %>/styles/voicebase-error.css': 'src/scss/voicebase-error.scss',
-                    '<%= distdir %>/styles/api-console-light-theme.css': 'src/scss/light-theme.scss',
-                    '<%= distdir %>/styles/api-console-dark-theme.css':  'src/scss/dark-theme.scss'
-                }
-            }
-        },
-
-        watch: {
-            dist: {
-                options: {
-                    livereload: true
-                },
-
-                tasks: [],
-                files: [
-                    '<%= distdir %>/**/*'
-                ]
-            },
-
-            scripts: {
-                tasks: ['build:scripts'],
-                files: [
-                    '<%= ngtemplates.ramlConsole.src %>',
-                    '<%= src.js %>'
-                ]
-            },
-
-            vendor: {
-                tasks: ['concat:scripts'],
-                files: [
-                    '<%= concat.vendor.src %>'
-                ]
-            },
-
-            index: {
-                tasks: ['concat:index'],
-                files: [
-                    '<%= concat.index.src %>'
-                ]
-            },
-
-            styles: {
-                tasks: ['build:styles'],
-                files: [
-                    'src/scss/**/*.scss'
-                ]
-            },
-
-            pages: {
-              tasks: ['copy:pages'],
-              files: [
-                'src/assets/pages/**/*.html'
-              ]
-            },
-
-            images: {
-              tasks: ['copy:images'],
-              files: [
-                'src/assets/img/**/*.*'
-              ]
-            }
-
-        },
-
-        /*jshint camelcase: false */
-        css_prefix: {
-            prefix: {
-                options: {
-                    prefix:      'raml-console-',
-                    processName: 'trim'
-                },
-
-                files: {
-                    '<%= distdir %>/styles/api-console-light-theme.css': '<%= distdir %>/styles/api-console-light-theme.css',
-                    '<%= distdir %>/styles/api-console-dark-theme.css':  '<%= distdir %>/styles/api-console-dark-theme.css'
-                }
-            }
-        },
-        /*jshint camelcase: true */
-
-        jshint: {
-            options: {
-                jshintrc: true
-            },
-
-            files: [
-                'Gruntfile.js',
-                '<%= src.js %>',
-                '<%= src.test %>'
-            ]
-        },
-
-        protractor: {
-            options: {
-                keepAlive:  false
-            },
-
-            apiConsole: {
-                options: {
-                    configFile: 'bower_components/api-console-voicebase/test/regression/local.protractor.conf.js'
-                }
-            },
-
-            voicebase: {
-                options: {
-                  configFile: 'test/regression/local.protractor.conf.js'
-                }
-            }
+      livereload: {
+        options: {
+          livereload: true,
+          open: true,
+          middleware: function (connect) {
+            return [
+              connect.static('dist')
+            ];
+          }
         }
+      },
 
-    });
+      regression: {
+        options: {
+          livereload: true,
+          open: false,
+          middleware: function (connect) {
+            return [
+              connect.static('dist'),
+              connect.static('test/regression/assets')
+            ];
+          }
+        }
+      }
 
-    grunt.registerTask('default', [
-        'build',
-        'connect:livereload',
-        'watch'
-    ]);
+    },
 
-    // build app with default mulesoft settings
-    grunt.registerTask('buildMulesoft', [
-        'jshint',
-        'clean',
-        'concurrent:buildMulesoft'
-    ]);
+    clean: {
+      build: [
+        '<%= tempdir %>',
+        '<%= distdir %>'
+      ]
+    },
 
-    // build app with voicebase settings
-    grunt.registerTask('build', [
-        'jshint',
-        'clean',
-        'concurrent:build'
-    ]);
+    copy: {
+      assets: {
+        files: [{
+          dest: '<%= distdir %>',
+          cwd: 'bower_components/api-console-voicebase/src/assets/',
+          expand: true,
+          src: [
+            '**',
+            '!styles/**/*'
+          ]
+        }]
+      },
 
-    grunt.registerTask('build:scripts', [
-        'ngtemplates',
-        'concat:app'
-    ]);
+      pages: {
+        files: [{
+          dest: '<%= distdir %>/pages',
+          cwd: 'src/assets/pages/',
+          expand: true,
+          src: [
+            '**'
+          ]
+        }]
+      },
 
-    grunt.registerTask('build:styles', [
-        'sass:build',
-        'css_prefix:prefix',
-        'concurrent:themes'
-    ]);
+      images: {
+        files: [{
+          dest: '<%= distdir %>/img',
+          cwd: 'src/assets/img/',
+          expand: true,
+          src: [
+            '**'
+          ]
+        }]
+      }
+    },
+
+    ngtemplates: {
+      ramlConsole: {
+        options: {
+          module: 'ramlConsoleApp'
+        },
+
+        cwd: 'src/app',
+        src: '**/*.tpl.html',
+        dest: '<%= tempdir %>/templates/app.js'
+      }
+    },
+
+    concat: {
+      app: {
+        dest: '<%= distdir %>/scripts/api-console.js',
+        src: [
+          '<%= src.js %>',
+          '<%= ngtemplates.ramlConsole.dest %>'
+        ]
+      },
+
+      index: {
+        options: {
+          process: true
+        },
+
+        dest: '<%= distdir %>/index.html',
+        src: 'src/voicebase.html'
+      },
+
+      indexMulesoft: {
+        options: {
+          process: true
+        },
+
+        dest: '<%= distdir %>/index.html',
+        src: 'src/index.html'
+      },
+
+      darkTheme: {
+        options: {
+          process: function process(value) {
+            return value.replace(/\.raml-console-CodeMirror/g, '.CodeMirror');
+          }
+        },
+
+        dest: '<%= distdir %>/styles/api-console-dark-theme.css',
+        src: [
+          'bower_components/api-console-voicebase/src/assets/styles/vendor/codemirror.css',
+          'bower_components/api-console-voicebase/src/assets/styles/fonts.css',
+          'bower_components/api-console-voicebase/src/assets/styles/error.css',
+          '<%= distdir %>/styles/voicebase-error.css',
+          '<%= distdir %>/styles/api-console-dark-theme.css',
+          'bower_components/api-console-voicebase/src/assets/styles/vendor/codemirror-dark.css'
+        ]
+      },
+
+      lightTheme: {
+        options: {
+          process: function process(value) {
+            return value.replace(/\.raml-console-CodeMirror/g, '.CodeMirror');
+          }
+        },
+
+        dest: '<%= distdir %>/styles/api-console-light-theme.css',
+        src: [
+          'bower_components/api-console-voicebase/src/assets/styles/vendor/codemirror.css',
+          'bower_components/api-console-voicebase/src/assets/styles/fonts.css',
+          'bower_components/api-console-voicebase/src/assets/styles/error.css',
+          '<%= distdir %>/styles/voicebase-error.css',
+          '<%= distdir %>/styles/api-console-light-theme.css',
+          'bower_components/api-console-voicebase/src/assets/styles/vendor/codemirror-light.css'
+        ]
+      },
+
+      vendor: {
+        src: '<%= src.jsVendor %>',
+        dest: '<%= distdir %>/scripts/api-console-vendor.js'
+      }
+
+    },
+
+    concurrent: {
+      build: [
+        'build:scripts',
+        'concat:vendor',
+        'concat:index',
+        'copy:assets',
+        'copy:pages',
+        'copy:images',
+        'build:styles'
+      ],
+
+      buildMulesoft: [
+        'build:scripts',
+        'concat:vendor',
+        'concat:indexMulesoft',
+        'copy:assets',
+        'build:styles'
+      ],
+
+      themes: [
+        'concat:darkTheme',
+        'concat:lightTheme'
+      ]
+    },
+
+    sass: {
+      build: {
+        options: {
+          sourcemap: 'none',
+          style: 'expanded'
+        },
+
+        files: {
+          '<%= distdir %>/styles/voicebase-error.css': 'src/scss/voicebase-error.scss',
+          '<%= distdir %>/styles/api-console-light-theme.css': 'src/scss/light-theme.scss',
+          '<%= distdir %>/styles/api-console-dark-theme.css': 'src/scss/dark-theme.scss'
+        }
+      },
+
+      min: {
+        options: {
+          sourcemap: 'none',
+          style: 'compressed'
+        },
+
+        files: {
+          '<%= distdir %>/styles/voicebase-error.css': 'src/scss/voicebase-error.scss',
+          '<%= distdir %>/styles/api-console-light-theme.css': 'src/scss/light-theme.scss',
+          '<%= distdir %>/styles/api-console-dark-theme.css': 'src/scss/dark-theme.scss'
+        }
+      }
+    },
+
+    watch: {
+      dist: {
+        options: {
+          livereload: true
+        },
+
+        tasks: [],
+        files: [
+          '<%= distdir %>/**/*'
+        ]
+      },
+
+      scripts: {
+        tasks: ['build:scripts'],
+        files: [
+          '<%= ngtemplates.ramlConsole.src %>',
+          '<%= src.js %>'
+        ]
+      },
+
+      vendor: {
+        tasks: ['concat:scripts'],
+        files: [
+          '<%= concat.vendor.src %>'
+        ]
+      },
+
+      index: {
+        tasks: ['concat:index'],
+        files: [
+          '<%= concat.index.src %>'
+        ]
+      },
+
+      styles: {
+        tasks: ['build:styles'],
+        files: [
+          'src/scss/**/*.scss'
+        ]
+      },
+
+      pages: {
+        tasks: ['copy:pages'],
+        files: [
+          'src/assets/pages/**/*.html'
+        ]
+      },
+
+      images: {
+        tasks: ['copy:images'],
+        files: [
+          'src/assets/img/**/*.*'
+        ]
+      }
+
+    },
+
+    /*jshint camelcase: false */
+    css_prefix: {
+      prefix: {
+        options: {
+          prefix: 'raml-console-',
+          processName: 'trim'
+        },
+
+        files: {
+          '<%= distdir %>/styles/api-console-light-theme.css': '<%= distdir %>/styles/api-console-light-theme.css',
+          '<%= distdir %>/styles/api-console-dark-theme.css': '<%= distdir %>/styles/api-console-dark-theme.css'
+        }
+      }
+    },
+    /*jshint camelcase: true */
+
+    jshint: {
+      options: {
+        jshintrc: true
+      },
+
+      files: [
+        'Gruntfile.js',
+        '<%= src.js %>',
+        '<%= src.test %>'
+      ]
+    },
+
+    protractor: {
+      options: {
+        keepAlive: false
+      },
+
+      apiConsole: {
+        options: {
+          configFile: 'bower_components/api-console-voicebase/test/regression/local.protractor.conf.js'
+        }
+      },
+
+      voicebase: {
+        options: {
+          configFile: 'test/regression/local.protractor.conf.js'
+        }
+      }
+    }
+
+  });
+
+  grunt.registerTask('default', [
+    'build',
+    'connect:livereload',
+    'watch'
+  ]);
+
+  // build app with default mulesoft settings
+  grunt.registerTask('buildMulesoft', [
+    'jshint',
+    'clean',
+    'concurrent:buildMulesoft'
+  ]);
+
+  // build app with voicebase settings
+  grunt.registerTask('build', [
+    'jshint',
+    'clean',
+    'concurrent:build'
+  ]);
+
+  grunt.registerTask('build:scripts', [
+    'ngtemplates',
+    'concat:app'
+  ]);
+
+  grunt.registerTask('build:styles', [
+    'sass:build',
+    'css_prefix:prefix',
+    'concurrent:themes'
+  ]);
 
   grunt.registerTask('regression', [
     'connect:regression',
