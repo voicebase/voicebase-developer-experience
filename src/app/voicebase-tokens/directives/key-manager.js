4e568d444bb911d3a17f767bc31d139c0d6c2009
pronin <denis.pronin@sibers.com>
2015-07-31 13:20:24 +0600

    [3322] - getting users and their tokens

--- src/app/voicebase-tokens/directives/key-manager.js
+++ src/app/voicebase-tokens/directives/key-manager.js
@@ -12,11 +12,42 @@
       controller: function($scope, voicebaseTokensApi, formValidate) {
         var me = this;
 
+        var credentials = {
+          username: '',
+          password: ''
+        };
+
         var tokenFromStorage = voicebaseTokensApi.getTokenFromStorage();
         var tokenData = voicebaseTokensApi.getCurrentToken();
         me.isLogin = (tokenData) ? true : false;
 
+        me.users = [];
 
+        me.getUsers = function () {
+            voicebaseTokensApi.getUsers(credentials).then(function (users) {
+
+              me.users = users;
+
+            }, function () {
+              me.errorMessage = 'Can\'t getting users!';
+            });
+        };
+
+        me.showUserTokens = function (user) {
+          if(!user.tokens) {
+            user.isLoadTokens = true;
+            voicebaseTokensApi.getUserTokens(credentials, user.userId).then(function (tokens) {
+              user.isLoadTokens = false;
+              user.tokens = tokens;
+            }, function () {
+              user.isLoadTokens = false;
+              me.errorMessage = 'Can\'t getting tokens!';
+            });
+
+          }
+        };
+
+        me.getUsers();
       }
     };
   };
