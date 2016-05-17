f8073e6b6985b6251f0bcdb653a865d708898c31
pronin <pronin.idenis@gmail.com>
2016-05-17 11:27:59 +0600

    [V2-1020] - add time to key "date created"

--- src/app/auth0/directives/autho-keys-list.js
+++ src/app/auth0/directives/autho-keys-list.js
@@ -48,9 +48,25 @@
             var day = dateObj.getDate();
             var month = months.getMonthById(dateObj.getMonth() + 1).short;
             var year = dateObj.getFullYear();
-            dateLabel = month + ' ' + day + ', ' + year;
+            var hours = dateObj.getHours();
+            var minutes = dateObj.getMinutes();
+            var ampm = hours >= 12 ? 'PM' : 'AM';
+            hours = hours % 12;
+            hours = hours ? hours : 12; // the hour '0' should be '12'
+            minutes = padLeft(minutes.toString(), 2);
+            dateLabel = month + ' ' + day + ', ' + year + ', ' + hours + ':' + minutes + ' ' + ampm;
           }
           return dateLabel;
+        };
+
+        var padLeft = function (string, total) {
+          if (typeof string !== 'string') {
+            throw new Error('First parameter must be a string');
+          }
+          if (typeof total !== 'number') {
+            throw new Error('Second parameter must be a integer');
+          }
+          return new Array(total - string.length + 1).join('0') + string;
         };
 
         me.onGenerateApiKey = function () {
