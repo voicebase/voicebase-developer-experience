050c55ad74f27b347e63763302f6349b278b9c9b
pronin <pronin.idenis@gmail.com>
2016-08-30 11:58:50 +0700

    [1580] - fix player plugin

--- dist/voicebase-player-lib/js/workers/localSearchWorker.js
+++ dist/voicebase-player-lib/js/workers/localSearchWorker.js
@@ -4,7 +4,7 @@
     importScripts('../lib/fuse.min.js', 'localSearchHelper.js');
 
     self.onmessage = function(e) {
-        var results = localSearchHelper.localTranscriptSearch(e.data.transcript, e.data.terms);
+        var results = localSearchHelper.localTranscriptSearch(e.data.transcript, e.data.terms, e.data.isApi2_0);
         self.postMessage({results: results});
     };
 
